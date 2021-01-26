dacab122aa0fb2af0dc737f17bb1762dcc7c2960
Gabriel R. Pinheiro <gabriel.pinheiro1995@gmail.com>
2021-01-25 22:58:27 -0300

    Nível 3 finalizado

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,74 @@
+import React, { createContext, useCallback, useState, useContext } from 'react';
+import api from '../services/api';
+
+interface AuthState {
+  token: string;
+  mappedUser: unknown;
+}
+
+interface SignInCredencials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  mappedUser: unknown;
+  signIn(credentials: SignInCredencials): Promise<void>;
+  signOut(): void;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token');
+    const mappedUser = localStorage.getItem('@GoBarber:mappedUser');
+
+    if (token && mappedUser) {
+      return { token, mappedUser: JSON.parse(mappedUser) };
+    }
+
+    return {} as AuthState;
+  });
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('sessions', {
+      email,
+      password,
+    });
+
+    const { token, mappedUser } = response.data;
+
+    localStorage.setItem('@GoBarber:token', token);
+    localStorage.setItem('@GoBarber:mappedUser', JSON.stringify(mappedUser));
+
+    setData({ token, mappedUser });
+  }, []);
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@GoBarber:token');
+    localStorage.removeItem('@GoBarber:mappedUser');
+
+    setData({} as AuthState);
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ mappedUser: data.mappedUser, signIn, signOut }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
